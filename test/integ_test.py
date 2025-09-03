e3db0e22fe441289b24304ecbf513c6f8b309646
Morgan Kuang <morgan.kuang@austin.utexas.edu>
2025-09-03 12:44:26 -0500

    Change manually opening and closing file to using context management.

--- test/integ_test.py
+++ test/integ_test.py
@@ -7,9 +7,9 @@ import automagician.main
 
 def test_automagician_lockfile_present():
     lockfile_path = "/tmp/automagician/" + os.environ["USER"] + "-lock"
-    lockfile = open(lockfile_path, "w")
-    lockfile.write("MockLockfile")
-    lockfile.close()
+    with open(lockfile_path, "w") as lockfile:
+        lockfile.write("MockLockfile")
+
     with pytest.raises(SystemExit):
         automagician.main.main()
     os.remove(lockfile_path)
