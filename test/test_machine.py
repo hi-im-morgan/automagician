72c0782908e52bfffda37a4260a7db175b732472
Morgan Kuang <morgan.kuang@austin.utexas.edu>
2025-09-14 15:06:46 -0500

    Add basic test coverage for machine.ssh_scp_init

--- test/test_machine.py
+++ test/test_machine.py
@@ -1,6 +1,6 @@
-from automagician.classes import Machine
-from automagician.machine import is_oden, is_tacc
-
+from automagician.classes import Machine, SSHConfig
+from automagician.machine import *
+import logging
 
 def test_is_oden():
     assert is_oden(Machine.FRI_ODEN)
@@ -18,3 +18,14 @@ def test_is_tacc():
     assert is_tacc(Machine.FRONTERRA_TACC)
     assert is_tacc(Machine.LS6_TACC)
     assert is_tacc(Machine.STAMPEDE2_TACC)
+
+def test_ssh_scp_init():
+    logger = logging.Logger("test")
+    ssh_config = ssh_scp_init(
+        machine = get_machine_number(),
+        home_dir = os.environ["HOME"],
+        balance = True,
+        logger = logger
+    )
+
+    assert isinstance(ssh_config, SSHConfig)
